6bf9562e4b7a0cd1c7bb2f8f35acc4a39af15803
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-09-04 10:59:42 +0900

    update

--- src/normal.rs
+++ src/normal.rs
@@ -2,21 +2,22 @@ use crate::Distribution;
 use rand::prelude::*;
 use rand_distr::Normal as RandNormal;
 
+#[derive(Debug)]
 pub struct Normal {
     mean: f64,
-    var: f64,
+    std_dev: f64,
 }
 
 impl Normal {
-    fn new(mean: f64, var: f64) -> Self {
-        Self { mean, var }
+    fn new(mean: f64, std_dev: f64) -> Self {
+        Self { mean, std_dev }
     }
 
     pub fn from(mean: f64, var: f64) -> Result<Self, String> {
         if var <= 0.0 {
             Err("variance must be greater than zero".to_owned())
         } else {
-            Ok(Self::new(mean, var))
+            Ok(Self::new(mean, var.sqrt()))
         }
     }
 
@@ -24,14 +25,18 @@ impl Normal {
         self.mean
     }
 
+    pub fn std_dev(&self) -> f64 {
+        self.std_dev
+    }
+
     pub fn var(&self) -> f64 {
-        self.var
+        self.std_dev.powi(2)
     }
 }
 
 impl Distribution for Normal {
     fn sample(&self, rng: &mut StdRng) -> Result<f64, String> {
-        let normal = match RandNormal::new(self.mean, self.var.sqrt()) {
+        let normal = match RandNormal::new(self.mean, self.std_dev) {
             Ok(n) => n,
             Err(_) => {
                 return Err("too small variance".to_owned());
