3669c36ae71d1fbaef903a5f0206c05e1b264dc8
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-08-10 12:24:33 +0900

    feat: student-t

--- src/continuous/multivariate/multivariate_student_t.rs
+++ src/continuous/multivariate/multivariate_student_t.rs
@@ -14,127 +14,136 @@ pub struct MultivariateStudentT;
 
 #[derive(thiserror::Error, Debug)]
 pub enum MultivariateStudentTError {
-  #[error("dimension mismatch")]
-  DimensionMismatch,
+    #[error("dimension mismatch")]
+    DimensionMismatch,
 }
 
 impl Distribution for MultivariateStudentT {
-  type T = Vec<f64>;
-  type U = MultivariateStudentTParams;
+    type T = Vec<f64>;
+    type U = MultivariateStudentTParams;
 
-  fn p(&self, x: &Self::T, theta: &Self::U) -> Result<f64, DistributionError> {
-    let mu = theta.mu();
-    let lsigma = theta.lsigma();
-    let nu = theta.nu();
+    fn p(&self, x: &Self::T, theta: &Self::U) -> Result<f64, DistributionError> {
+        let nu = theta.nu();
+        let mu = theta.mu();
+        let lsigma = theta.lsigma();
 
-    let p = x.len();
+        let n = x.len();
 
-    if p != mu.len() {
-      return Err(DistributionError::InvalidParameters(
-        MultivariateStudentTError::DimensionMismatch.into(),
-      ));
+        if n != mu.len() {
+            return Err(DistributionError::InvalidParameters(
+                MultivariateStudentTError::DimensionMismatch.into(),
+            ));
+        }
+        let n = n as f64;
+        let nu = nu;
+
+        let x_mu = x
+            .par_iter()
+            .zip(mu.par_iter())
+            .map(|(&xi, &mui)| xi - mui)
+            .collect::<Vec<_>>()
+            .col_mat();
+
+        Ok((Gamma::gamma((nu + n) / 2.0)
+            / (Gamma::gamma(nu / 2.0) * nu.powf(n / 2.0) * PI.powf(n / 2.0) * lsigma.trdet()))
+            * (1.0 + (x_mu.t() * lsigma.potrs(x_mu)?)[0][0] / nu).powf(-(nu + n) / 2.0))
     }
-    let p = p as f64;
-    let nu = nu;
 
-    let x_mu = x
-      .par_iter()
-      .zip(mu.par_iter())
-      .map(|(&xi, &mui)| xi - mui)
-      .collect::<Vec<_>>()
-      .col_mat();
+    fn sample(&self, theta: &Self::U, rng: &mut StdRng) -> Result<Self::T, DistributionError> {
+        let nu = theta.nu();
+        let mu = theta.mu();
+        let lsigma = theta.lsigma();
 
-    Ok(
-      (Gamma::gamma((nu + p) / 2.0)
-        / (Gamma::gamma(nu / 2.0) * nu.powf(p / 2.0) * PI.powf(p / 2.0) * lsigma.trdet()))
-        * (1.0 + (x_mu.t() * lsigma.potrs(x_mu)?)[0][0] / nu).powf(-(nu + p) / 2.0),
-    )
-  }
+        let student_t = match RandStudentT::new(nu as f64) {
+            Ok(v) => Ok(v),
+            Err(e) => Err(DistributionError::Others(e.into())),
+        }?;
 
-  fn sample(&self, theta: &Self::U, rng: &mut StdRng) -> Result<Self::T, DistributionError> {
-    let mu = theta.mu();
-    let lsigma = theta.lsigma();
-    let nu = theta.nu();
+        let z = (0..lsigma.rows())
+            .into_iter()
+            .map(|_| rng.sample(student_t))
+            .collect::<Vec<_>>();
 
-    let student_t = match RandStudentT::new(nu as f64) {
-      Ok(v) => Ok(v),
-      Err(e) => Err(DistributionError::Others(e.into())),
-    }?;
+        let y = mu.clone().col_mat().gemm(lsigma, &z.col_mat(), 1.0, 1.0)?;
 
-    let z = (0..lsigma.rows())
-      .into_iter()
-      .map(|_| rng.sample(student_t))
-      .collect::<Vec<_>>();
-
-    let y = mu.clone().col_mat().gemm(lsigma, &z.col_mat(), 1.0, 1.0)?;
-
-    Ok(y.vec())
-  }
+        Ok(y.vec())
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct MultivariateStudentTParams {
-  mu: Vec<f64>,
-  lsigma: Matrix,
-  nu: f64,
+    nu: f64,
+    mu: Vec<f64>,
+    lsigma: Matrix,
 }
 
 impl MultivariateStudentTParams {
-  /// # Multivariate student t
-  /// `L` is needed as second argument under decomposition `Sigma = L * L^T`
-  /// lsigma = sigma.potrf()?;
-  pub fn new(mu: Vec<f64>, lsigma: Matrix, nu: f64) -> Result<Self, DistributionError> {
-    let n = mu.len();
-    if n != lsigma.rows() || n != lsigma.cols() {
-      return Err(DistributionError::InvalidParameters(
-        MultivariateStudentTError::DimensionMismatch.into(),
-      ));
+    /// # Multivariate student t
+    /// `L` is needed as second argument under decomposition `Sigma = L * L^T`
+    /// lsigma = sigma.potrf()?;
+    pub fn new(nu: f64, mu: Vec<f64>, lsigma: Matrix) -> Result<Self, DistributionError> {
+        let n = mu.len();
+        if n != lsigma.rows() || n != lsigma.cols() {
+            return Err(DistributionError::InvalidParameters(
+                MultivariateStudentTError::DimensionMismatch.into(),
+            ));
+        }
+
+        Ok(Self { nu, mu, lsigma })
     }
 
-    Ok(Self { mu, lsigma, nu })
-  }
+    pub fn nu(&self) -> f64 {
+        self.nu
+    }
 
-  pub fn mu(&self) -> &Vec<f64> {
-    &self.mu
-  }
+    pub fn mu(&self) -> &Vec<f64> {
+        &self.mu
+    }
 
-  pub fn lsigma(&self) -> &Matrix {
-    &self.lsigma
-  }
+    pub fn lsigma(&self) -> &Matrix {
+        &self.lsigma
+    }
+}
 
-  pub fn nu(&self) -> f64 {
-    self.nu
-  }
+pub struct MultivariateCauchyParams;
+
+impl MultivariateCauchyParams {
+    pub fn new(
+        mu: Vec<f64>,
+        lsigma: Matrix,
+    ) -> Result<MultivariateStudentTParams, DistributionError> {
+        MultivariateStudentTParams::new(1.0, mu, lsigma)
+    }
 }
 
 impl<Rhs, TRhs> Mul<Rhs> for MultivariateStudentT
 where
-  Rhs: Distribution<T = TRhs, U = MultivariateStudentTParams>,
-  TRhs: RandomVariable,
+    Rhs: Distribution<T = TRhs, U = MultivariateStudentTParams>,
+    TRhs: RandomVariable,
 {
-  type Output = IndependentJoint<Self, Rhs, Vec<f64>, TRhs, MultivariateStudentTParams>;
+    type Output = IndependentJoint<Self, Rhs, Vec<f64>, TRhs, MultivariateStudentTParams>;
 
-  fn mul(self, rhs: Rhs) -> Self::Output {
-    IndependentJoint::new(self, rhs)
-  }
+    fn mul(self, rhs: Rhs) -> Self::Output {
+        IndependentJoint::new(self, rhs)
+    }
 }
 
 impl<Rhs, URhs> BitAnd<Rhs> for MultivariateStudentT
 where
-  Rhs: Distribution<T = MultivariateStudentTParams, U = URhs>,
-  URhs: RandomVariable,
+    Rhs: Distribution<T = MultivariateStudentTParams, U = URhs>,
+    URhs: RandomVariable,
 {
-  type Output = DependentJoint<Self, Rhs, Vec<f64>, MultivariateStudentTParams, URhs>;
+    type Output = DependentJoint<Self, Rhs, Vec<f64>, MultivariateStudentTParams, URhs>;
 
-  fn bitand(self, rhs: Rhs) -> Self::Output {
-    DependentJoint::new(self, rhs)
-  }
+    fn bitand(self, rhs: Rhs) -> Self::Output {
+        DependentJoint::new(self, rhs)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
-  }
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
--- src/continuous/student_t.rs
+++ src/continuous/student_t.rs
@@ -12,76 +12,104 @@ use std::{ops::BitAnd, ops::Mul};
 pub struct StudentT;
 
 #[derive(thiserror::Error, Debug)]
-pub enum StudentTError {}
+pub enum StudentTError {
+    #[error("'σ' must be positive")]
+    SigmaMustBePositive,
+}
 
 impl Distribution for StudentT {
-  type T = f64;
-  type U = StudentTParams;
+    type T = f64;
+    type U = StudentTParams;
 
-  fn p(&self, x: &Self::T, theta: &Self::U) -> Result<f64, DistributionError> {
-    let nu = theta.nu();
+    fn p(&self, x: &Self::T, theta: &Self::U) -> Result<f64, DistributionError> {
+        let nu = theta.nu();
+        let mu = theta.mu();
+        let sigma = theta.sigma();
 
-    Ok(
-      (Gamma::gamma((nu + 1.0) / 2.0) / ((nu * PI).sqrt() * Gamma::gamma(nu / 2.0)))
-        * (1.0 + x.powi(2) / nu).powf(-((nu + 1.0) / 2.0)),
-    )
-  }
+        Ok(
+            (Gamma::gamma((nu + 1.0) / 2.0) / ((nu * PI).sqrt() * Gamma::gamma(nu / 2.0)))
+                * (1.0 + ((x - mu) / sigma).powi(2) / nu).powf(-((nu + 1.0) / 2.0)),
+        )
+    }
 
-  fn sample(&self, theta: &Self::U, rng: &mut StdRng) -> Result<Self::T, DistributionError> {
-    let nu = theta.nu();
+    fn sample(&self, theta: &Self::U, rng: &mut StdRng) -> Result<Self::T, DistributionError> {
+        let nu = theta.nu();
 
-    let student_t = match RandStudentT::new(nu) {
-      Ok(v) => Ok(v),
-      Err(e) => Err(DistributionError::Others(e.into())),
-    }?;
+        let student_t = match RandStudentT::new(nu) {
+            Ok(v) => Ok(v),
+            Err(e) => Err(DistributionError::Others(e.into())),
+        }?;
 
-    Ok(rng.sample(student_t))
-  }
+        Ok(rng.sample(student_t))
+    }
 }
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct StudentTParams {
-  nu: f64,
+    nu: f64,
+    mu: f64,
+    sigma: f64,
 }
 
 impl StudentTParams {
-  pub fn new(nu: f64) -> Result<Self, DistributionError> {
-    Ok(Self { nu })
-  }
+    pub fn new(nu: f64, mu: f64, sigma: f64) -> Result<Self, DistributionError> {
+        if sigma <= 0.0 {
+            return Err(DistributionError::InvalidParameters(
+                StudentTError::SigmaMustBePositive.into(),
+            ));
+        }
+        Ok(Self { nu, mu, sigma })
+    }
 
-  pub fn nu(&self) -> f64 {
-    self.nu
-  }
+    pub fn nu(&self) -> f64 {
+        self.nu
+    }
+
+    pub fn mu(&self) -> f64 {
+        self.mu
+    }
+
+    pub fn sigma(&self) -> f64 {
+        self.sigma
+    }
+}
+
+pub struct CauchyParams;
+
+impl CauchyParams {
+    pub fn new(mu: f64, sigma: f64) -> Result<StudentTParams, DistributionError> {
+        StudentTParams::new(1.0, mu, sigma)
+    }
 }
 
 impl<Rhs, TRhs> Mul<Rhs> for StudentT
 where
-  Rhs: Distribution<T = TRhs, U = StudentTParams>,
-  TRhs: RandomVariable,
+    Rhs: Distribution<T = TRhs, U = StudentTParams>,
+    TRhs: RandomVariable,
 {
-  type Output = IndependentJoint<Self, Rhs, f64, TRhs, StudentTParams>;
+    type Output = IndependentJoint<Self, Rhs, f64, TRhs, StudentTParams>;
 
-  fn mul(self, rhs: Rhs) -> Self::Output {
-    IndependentJoint::new(self, rhs)
-  }
+    fn mul(self, rhs: Rhs) -> Self::Output {
+        IndependentJoint::new(self, rhs)
+    }
 }
 
 impl<Rhs, URhs> BitAnd<Rhs> for StudentT
 where
-  Rhs: Distribution<T = StudentTParams, U = URhs>,
-  URhs: RandomVariable,
+    Rhs: Distribution<T = StudentTParams, U = URhs>,
+    URhs: RandomVariable,
 {
-  type Output = DependentJoint<Self, Rhs, f64, StudentTParams, URhs>;
+    type Output = DependentJoint<Self, Rhs, f64, StudentTParams, URhs>;
 
-  fn bitand(self, rhs: Rhs) -> Self::Output {
-    DependentJoint::new(self, rhs)
-  }
+    fn bitand(self, rhs: Rhs) -> Self::Output {
+        DependentJoint::new(self, rhs)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
-  }
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
