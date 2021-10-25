8da3728ac596f1b1019324479a2209437042cc0a
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-25 23:55:24 +0900

    feat: vector sampleable

--- src/mcmc/mod.rs
+++ src/mcmc/mod.rs
@@ -1,3 +1,49 @@
 pub mod elliptical_slice;
 
 pub use elliptical_slice::*;
+use opensrdk_linear_algebra::Matrix;
+
+use crate::RandomVariable;
+
+pub trait VectorSampleable: RandomVariable {
+    type T;
+    fn transform_vec(self) -> (Vec<f64>, Self::T);
+    fn restore(v: (Vec<f64>, Self::T)) -> Self;
+}
+
+impl VectorSampleable for f64 {
+    type T = ();
+
+    fn transform_vec(self) -> (Vec<f64>, Self::T) {
+        (vec![self], ())
+    }
+
+    fn restore(v: (Vec<f64>, Self::T)) -> Self {
+        v.0[0]
+    }
+}
+
+impl VectorSampleable for Vec<f64> {
+    type T = ();
+
+    fn transform_vec(self) -> (Vec<f64>, Self::T) {
+        (self, ())
+    }
+
+    fn restore(v: (Vec<f64>, Self::T)) -> Self {
+        v.0
+    }
+}
+
+impl VectorSampleable for Matrix {
+    type T = usize;
+
+    fn transform_vec(self) -> (Vec<f64>, Self::T) {
+        let rows = self.rows();
+        (self.vec(), rows)
+    }
+
+    fn restore(v: (Vec<f64>, Self::T)) -> Self {
+        Matrix::from(v.1, v.0)
+    }
+}
