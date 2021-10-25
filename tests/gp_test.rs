d348353dc5a470520a09fc0010be6197375135fc
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-25 11:17:48 +0900

    test: kiss love

--- tests/gp_test.rs
+++ tests/gp_test.rs
@@ -14,18 +14,25 @@ use rand::prelude::*;
 use rand_distr::StandardNormal;
 use std::time::Instant;
 
+#[derive(Clone, Copy)]
+pub enum Type {
+    Exact,
+    Sparse,
+    KissLove,
+}
+
 #[test]
 fn test_main() {
     let is_not_ci = false;
-    let is_gif = true;
-    let exact = false;
+    let is_gif = false;
+    let tp = Type::Sparse;
 
     if is_not_ci {
         let start = Instant::now();
         if is_gif {
-            draw_gif(exact).unwrap();
+            draw_gif(tp).unwrap();
         } else {
-            draw_png(exact).unwrap();
+            draw_png(tp).unwrap();
         }
         let end = start.elapsed();
         println!("{}.{:03}sec", end.as_secs(), end.subsec_nanos() / 1_000_000);
@@ -52,7 +59,7 @@ fn samples(size: usize) -> Vec<(f64, f64)> {
 }
 
 fn draw(
-    exact: bool,
+    tp: Type,
     size: usize,
     root: &DrawingArea<BitMapBackend, Shift>,
 ) -> Result<(), Box<dyn std::error::Error>> {
@@ -62,7 +69,6 @@ fn draw(
     let kernel = RBF + Periodic;
     let theta = vec![1.0; kernel.params_len()];
     let sigma = 1.0;
-    let base = BaseEllipticalProcessParams::new(kernel, x, theta, sigma)?;
 
     let x_axis = (-8.0..8.0).step(0.1);
 
@@ -93,36 +99,54 @@ fn draw(
         &|&coord, size, style| EmptyElement::at(coord) + Circle::new((0, 0), size, style),
     ))?;
 
-    let result = if exact {
-        let params = base.exact(&y)?;
-        x_axis
-            .values()
-            .map(|xs: f64| {
-                let np = params.gp_predict(&vec![xs]).unwrap();
-                let mu = np.mu();
-                let sigma = np.sigma();
+    let result = match tp {
+        Type::Exact => {
+            let params = BaseEllipticalProcessParams::new(kernel, x, theta, sigma)?.exact(&y)?;
+            x_axis
+                .values()
+                .map(|xs: f64| {
+                    let np = params.gp_predict(&vec![xs]).unwrap();
+                    let mu = np.mu();
+                    let sigma = np.sigma();
 
-                (xs, mu, sigma)
-            })
-            .collect::<Vec<_>>()
-    } else {
-        let params = base.sparse(
-            &y,
-            (0..=30)
-                .into_iter()
-                .map(|v| vec![v as f64 * 16.0 / 30.0 - 8.0])
-                .collect::<Vec<_>>(),
-        )?;
-        x_axis
-            .values()
-            .map(|xs: f64| {
-                let np = params.cp_predict(&vec![xs]).unwrap();
-                let mu = np.mu();
-                let sigma = np.sigma();
+                    (xs, mu, sigma)
+                })
+                .collect::<Vec<_>>()
+        }
+        Type::Sparse => {
+            let params = BaseEllipticalProcessParams::new(kernel, x, theta, sigma)?.sparse(
+                &y,
+                (0..=30)
+                    .into_iter()
+                    .map(|v| vec![v as f64 * 16.0 / 30.0 - 8.0])
+                    .collect::<Vec<_>>(),
+            )?;
+            x_axis
+                .values()
+                .map(|xs: f64| {
+                    let np = params.cp_predict(&vec![xs]).unwrap();
+                    let mu = np.mu();
+                    let sigma = np.sigma();
 
-                (xs, mu, sigma)
-            })
-            .collect::<Vec<_>>()
+                    (xs, mu, sigma)
+                })
+                .collect::<Vec<_>>()
+        }
+        Type::KissLove => {
+            let params =
+                BaseEllipticalProcessParams::new(Convolutional::new(kernel), x, theta, sigma)?
+                    .kiss_love(&y)?;
+            x_axis
+                .values()
+                .map(|xs: f64| {
+                    let np = params.gp_predict(&vec![xs]).unwrap();
+                    let mu = np.mu();
+                    let sigma = np.sigma();
+
+                    (xs, mu, sigma)
+                })
+                .collect::<Vec<_>>()
+        }
     };
 
     // chart.draw_series(AreaSeries::new(
@@ -152,28 +176,28 @@ fn draw(
     Ok(())
 }
 
-fn draw_png(exact: bool) -> Result<(), Box<dyn std::error::Error>> {
+fn draw_png(tp: Type) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::new(
-        if exact {
-            "exact_gp.png"
-        } else {
-            "sparse_cp.png"
+        match tp {
+            Type::Exact => "exact_gp.png",
+            Type::Sparse => "sparse_cp.png",
+            Type::KissLove => "kiss_love_gp.png",
         },
         (1600, 900),
     )
     .into_drawing_area();
 
-    draw(exact, 2usize.pow(10 + 3), &root)?;
+    draw(tp, 2usize.pow(10 + 3), &root)?;
 
     Ok(())
 }
 
-fn draw_gif(exact: bool) -> Result<(), Box<dyn std::error::Error>> {
+fn draw_gif(tp: Type) -> Result<(), Box<dyn std::error::Error>> {
     let root = BitMapBackend::gif(
-        if exact {
-            "exact_gp.gif"
-        } else {
-            "sparse_cp.gif"
+        match tp {
+            Type::Exact => "exact_gp.gif",
+            Type::Sparse => "sparse_cp.gif",
+            Type::KissLove => "kiss_love_gp.gif",
         },
         (1600, 900),
         1_000,
@@ -182,7 +206,7 @@ fn draw_gif(exact: bool) -> Result<(), Box<dyn std::error::Error>> {
 
     for k in 0..8 {
         println!("iter: {}", k);
-        draw(exact, 2usize.pow(3 + k), &root)?;
+        draw(tp, 2usize.pow(3 + k), &root)?;
     }
 
     Ok(())
