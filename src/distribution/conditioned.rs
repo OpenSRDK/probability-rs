a502e8d1ef8881018e2ab8197dbadefac103c5d6
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-29 16:11:36 +0900

    feat: conditioned

--- src/distribution/conditioned.rs
+++ src/distribution/conditioned.rs
@@ -1,5 +1,8 @@
-use crate::{Distribution, DistributionError, RandomVariable};
-use std::fmt::Debug;
+use crate::{DependentJoint, Distribution, DistributionError, IndependentJoint, RandomVariable};
+use std::{
+    fmt::Debug,
+    ops::{BitAnd, Mul},
+};
 
 #[derive(Clone)]
 pub struct ConditionedDistribution<'a, D, T, U1, U2>
@@ -9,7 +12,7 @@ where
     U1: RandomVariable,
     U2: RandomVariable,
 {
-    distribution: &'a D,
+    distribution: D,
     condition: &'a (dyn Fn(&U2) -> Result<U1, DistributionError> + Send + Sync),
 }
 
@@ -21,7 +24,7 @@ where
     U2: RandomVariable,
 {
     pub fn new(
-        distribution: &'a D,
+        distribution: D,
         condition: &'a (dyn Fn(&U2) -> Result<U1, DistributionError> + Send + Sync),
     ) -> Self {
         Self {
@@ -56,7 +59,6 @@ where
 {
     type T = T;
     type U = U2;
-
     fn p(&self, x: &Self::T, theta: &Self::U) -> Result<f64, crate::DistributionError> {
         self.distribution.p(x, &(self.condition)(theta)?)
     }
@@ -67,5 +69,63 @@ where
         rng: &mut rand::prelude::StdRng,
     ) -> Result<Self::T, crate::DistributionError> {
         self.distribution.sample(&(self.condition)(theta)?, rng)
+    }
+}
+
+pub trait ConditionableDistribution: Distribution + Sized {
+    fn condition<'a, U2>(
+        self,
+        condition: &'a (dyn Fn(&U2) -> Result<Self::U, DistributionError> + Send + Sync),
+    ) -> ConditionedDistribution<'a, Self, Self::T, Self::U, U2>
+    where
+        U2: RandomVariable;
+}
+
+impl<D, T, U1> ConditionableDistribution for D
+where
+    D: Distribution<T = T, U = U1>,
+    T: RandomVariable,
+    U1: RandomVariable,
+{
+    fn condition<'a, U2>(
+        self,
+        condition: &'a (dyn Fn(&U2) -> Result<Self::U, DistributionError> + Send + Sync),
+    ) -> ConditionedDistribution<'a, Self, Self::T, Self::U, U2>
+    where
+        U2: RandomVariable,
+    {
+        ConditionedDistribution::<Self, Self::T, Self::U, U2>::new(self, condition)
+    }
+}
+
+impl<'a, D, T, U1, U2, Rhs, TRhs> Mul<Rhs> for ConditionedDistribution<'a, D, T, U1, U2>
+where
+    D: Distribution<T = T, U = U1>,
+    T: RandomVariable,
+    U1: RandomVariable,
+    U2: RandomVariable,
+    Rhs: Distribution<T = TRhs, U = U2>,
+    TRhs: RandomVariable,
+{
+    type Output = IndependentJoint<Self, Rhs, T, TRhs, U2>;
+
+    fn mul(self, rhs: Rhs) -> Self::Output {
+        IndependentJoint::new(self, rhs)
+    }
+}
+
+impl<'a, D, T, U1, U2, Rhs, URhs> BitAnd<Rhs> for ConditionedDistribution<'a, D, T, U1, U2>
+where
+    D: Distribution<T = T, U = U1>,
+    T: RandomVariable,
+    U1: RandomVariable,
+    U2: RandomVariable,
+    Rhs: Distribution<T = U2, U = URhs>,
+    URhs: RandomVariable,
+{
+    type Output = DependentJoint<Self, Rhs, T, U2, URhs>;
+
+    fn bitand(self, rhs: Rhs) -> Self::Output {
+        DependentJoint::new(self, rhs)
     }
 }
