d653f0eeb1b5bac2a3af584cadd0e3bf751e2905
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-11-12 22:58:29 +0900

    feat: importance sampling

--- src/mcmc/importance_sampling.rs
+++ src/mcmc/importance_sampling.rs
@@ -1,15 +1,14 @@
 use crate::{Distribution, DistributionError, RandomVariable};
-use rand::prelude::*;
 
 /// Sample b from posterior p(b|a) with likelihood p(a|b) and prior p(b)
-/// Unbounded Slice Sampling
-/// http://chasen.org/~daiti-m/diary/?201510
-pub struct ImportanceSampler<L, P, A, PD>
+
+pub struct ImportanceSampler<L, P, A, B, PD>
 where
-    L: Distribution<T = A, U = f64>,
-    P: Distribution<T = f64, U = ()>,
+    L: Distribution<T = A, U = B>,
+    P: Distribution<T = B, U = ()>,
     A: RandomVariable,
-    PD: Distribution<T = f64, U = ()>,
+    B: RandomVariable,
+    PD: Distribution<T = B, U = ()>,
 {
     value: A,
     likelihood: L,
@@ -25,12 +24,13 @@ pub enum ImportanceSamplingError {
     Unknown,
 }
 
-impl<L, P, A, PD> ImportanceSampler<L, P, A, PD>
+impl<L, P, A, B, PD> ImportanceSampler<L, P, A, B, PD>
 where
-    L: Distribution<T = A, U = f64>,
-    P: Distribution<T = f64, U = ()>,
+    L: Distribution<T = A, U = B>,
+    P: Distribution<T = B, U = ()>,
     A: RandomVariable,
-    PD: Distribution<T = f64, U = ()>,
+    B: RandomVariable,
+    PD: Distribution<T = B, U = ()>,
 {
     pub fn new(value: A, likelihood: L, prior: P, proposal: PD) -> Result<Self, DistributionError> {
         Ok(Self {
@@ -41,45 +41,19 @@ where
         })
     }
 
-    pub fn sample(
-        &self,
-        x: f64,
-        max_iter: usize,
-        rng: &mut dyn RngCore,
-    ) -> Result<f64, DistributionError> {
-        let mut st = 0.0;
-        let mut ed = 1.0;
+    pub fn expectation(&self, f: impl Fn(&B) -> f64, x: &[B]) -> Result<f64, DistributionError> {
+        let wi_fxi = x
+            .iter()
+            .map(|xi| -> Result<_, DistributionError> {
+                let wi = self.likelihood.fk(&self.value, &xi)? * self.prior.fk(&xi, &())?
+                    / self.proposal.fk(&xi, &())?;
+                let fxi = f(xi);
+                Ok((wi, fxi))
+            })
+            .collect::<Result<Vec<_>, _>>()?;
+        let sum_w = wi_fxi.iter().map(|&(wi, _)| wi).sum::<f64>();
+        let sum_w_fx = wi_fxi.iter().map(|&(wi, fxi)| wi * fxi).sum::<f64>();
 
-        let r = shrink(x)?;
-        let slice = self.likelihood.fk(&self.value, &x)? * self.prior.fk(&x, &())?
-            - 2.0 * rng.gen_range(0.0f64..1.0f64).ln();
-
-        for _iter in 0..max_iter {
-            let rnew = rng.gen_range(st..ed);
-            let expanded = expand(rnew)?;
-
-            let newlik = self.likelihood.fk(&self.value, &expanded)?
-                * self.prior.fk(&expanded, &())?
-                - (2.0 * rnew * (1.0 - rnew));
-
-            if newlik > slice {
-                return expand(rnew);
-            } else if rnew > r {
-                ed = rnew;
-            } else if rnew < r {
-                st = rnew;
-            } else {
-                return Ok(x);
-            }
-        }
-        Ok(x)
+        Ok(sum_w_fx / sum_w)
     }
-}
-
-fn expand(p: f64) -> Result<f64, DistributionError> {
-    Ok(-100.0 * (1.0 / (p - 1.0)).ln())
-}
-
-fn shrink(x: f64) -> Result<f64, DistributionError> {
-    Ok(1.0 / (1.0 + (-x / 100.0).exp()))
 }
