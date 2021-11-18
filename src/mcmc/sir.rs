d32c70fb6beabb103765524d81ab4698cbdf29ad
Rubens <91782701+tibetsou@users.noreply.github.com>
2021-11-18 22:29:11 +0900

    feat: SIR作成中、スタートラインまで完成

--- src/mcmc/sir.rs
+++ src/mcmc/sir.rs
@@ -2,18 +2,18 @@
 use crate::rand::SeedableRng;
 use crate::{Distribution, DistributionError, RandomVariable, SamplesDistribution};
 use rand::rngs::StdRng;
+use std::hash::Hash;
 
 pub struct ParticleFilter<Y, X, D1, D2, PD>
 where
     Y: RandomVariable,
-    X: RandomVariable,
+    X: RandomVariable + Eq + Hash,
     D1: Distribution<Value = Y, Condition = X>,
     D2: Distribution<Value = X, Condition = X>,
     PD: Distribution<Value = X, Condition = X>,
 {
     value: Y,
     state: X,
-    particles: usize,
     distr_y: D1,
     distr_x: D2,
     proposal: PD,
@@ -22,7 +22,7 @@ where
 impl<Y, X, D1, D2, PD> ParticleFilter<Y, X, D1, D2, PD>
 where
     Y: RandomVariable,
-    X: RandomVariable,
+    X: RandomVariable + Eq + Hash,
     D1: Distribution<Value = Y, Condition = X>,
     D2: Distribution<Value = X, Condition = X>,
     PD: Distribution<Value = X, Condition = X>,
@@ -30,7 +30,6 @@ where
     pub fn new(
         value: Y,
         state: X,
-        particles: usize,
         distr_y: D1,
         distr_x: D2,
         proposal: PD,
@@ -38,7 +37,6 @@ where
         Ok(Self {
             value,
             state,
-            particles,
             distr_y,
             distr_x,
             proposal,
@@ -47,26 +45,23 @@ where
 
     pub fn filtering(
         &self,
-        f: impl Fn(&X) -> X,
-        h: impl Fn(&X) -> Y,
+        particles: usize,
         thr: f64,
-    ) -> Result<f64, DistributionError> {
+    ) -> Result<SamplesDistribution<X>, DistributionError> {
         let mut rng = StdRng::from_seed([1; 32]);
 
-        //let mut state = self.state.clone();
-
-        let mut p = (0..self.particles)
+        let mut p = (0..particles)
             .into_iter()
-            .map(|i| -> Result<_, DistributionError> {
+            .map(|_i| -> Result<_, DistributionError> {
                 let pi = self.proposal.sample(&self.state, &mut rng)?;
                 Ok(pi)
             })
             .collect::<Result<Vec<_>, _>>()?;
 
         loop {
-            let mut w_initial = vec![1.0 / self.particles as f64; self.particles];
+            let w_initial = vec![1.0 / particles as f64; particles];
 
-            let mut w_orig = (0..self.particles)
+            let w_orig = (0..particles)
                 .into_iter()
                 .map(|i| -> Result<_, DistributionError> {
                     let wi_orig = w_initial[i]
@@ -77,7 +72,7 @@ where
                 })
                 .collect::<Result<Vec<_>, _>>()?;
 
-            let mut w = (0..self.particles)
+            let w = (0..particles)
                 .into_iter()
                 .map(|i| -> Result<_, DistributionError> {
                     let wi = w_initial[i] / (w_orig.iter().map(|wi_orig| wi_orig).sum::<f64>());
@@ -85,29 +80,32 @@ where
                 })
                 .collect::<Result<Vec<_>, _>>()?;
 
-            let mut eff = 1.0 / (w.iter().map(|wi| wi.powi(2)).sum::<f64>());
+            let eff = 1.0 / (w.iter().map(|wi| wi.powi(2)).sum::<f64>());
 
             if eff > thr {
                 break;
             }
+
             let mut p_sample = vec![];
 
             for i in 0..w.len() {
-                let mut num_w = (1000.0 * w[i]).round() as usize;
-                let mut pi_sample = vec![p[i]; num_w];
-                let mut p_sample = p_sample.append(&mut pi_sample);
+                let num_w = (particles as f64 * 10.0 * w[i]).round() as usize;
+                let mut pi_sample = vec![p[i].clone(); num_w];
+                p_sample.append(&mut pi_sample);
             }
 
-            let mut weighted_distr = SamplesDistribution::new(p_sample);
-            let mut p = (0..self.particles)
+            let weighted_distr = SamplesDistribution::new(p_sample);
+            p = (0..particles)
                 .into_iter()
-                .map(|i| -> Result<_, DistributionError> {
+                .map(|_i| -> Result<_, DistributionError> {
                     let pi = weighted_distr.sample(&(), &mut rng)?;
                     Ok(pi)
                 })
                 .collect::<Result<Vec<_>, _>>()?;
         }
 
-        Ok(Y)
+        let x_distr = SamplesDistribution::new(p);
+
+        Ok(x_distr)
     }
 }
