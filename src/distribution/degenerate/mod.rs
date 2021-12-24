ea89e9e71ba85ec450351a965cff4cc7974afac5
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-12-24 22:31:22 +0900

    feat: degenerate

--- src/distribution/degenerate/mod.rs
+++ src/distribution/degenerate/mod.rs
@@ -1,35 +1,59 @@
+use std::{fmt::Debug, marker::PhantomData};
+
 use crate::{Distribution, RandomVariable};
 
-#[derive(Clone, Debug)]
-pub struct Degenerate<T>
+#[derive(Clone)]
+pub struct Degenerate<T, U, F>
 where
     T: RandomVariable + PartialEq,
+    U: RandomVariable,
+    F: Fn(&U) -> T + Clone + Send + Sync,
 {
-    value: T,
+    f: F,
+    phantom: PhantomData<(T, U)>,
 }
 
-impl<T> Degenerate<T>
+impl<T, U, F> Degenerate<T, U, F>
 where
     T: RandomVariable + PartialEq,
+    U: RandomVariable,
+    F: Fn(&U) -> T + Clone + Send + Sync,
 {
-    pub fn new(value: T) -> Self {
-        Self { value }
+    pub fn new(f: F) -> Self {
+        Self {
+            f,
+            phantom: PhantomData,
+        }
     }
 }
 
-impl<T> Distribution for Degenerate<T>
+impl<T, U, F> Debug for Degenerate<T, U, F>
 where
     T: RandomVariable + PartialEq,
+    U: RandomVariable,
+    F: Fn(&U) -> T + Clone + Send + Sync,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "DegenerateDistribution {{ }}",)
+    }
+}
+
+impl<T, U, F> Distribution for Degenerate<T, U, F>
+where
+    T: RandomVariable + PartialEq,
+    U: RandomVariable,
+    F: Fn(&U) -> T + Clone + Send + Sync,
 {
     type Value = T;
-    type Condition = ();
+    type Condition = U;
 
     fn fk(
         &self,
         x: &Self::Value,
-        _theta: &Self::Condition,
+        theta: &Self::Condition,
     ) -> Result<f64, crate::DistributionError> {
-        if x.eq(&self.value) {
+        let value = (self.f)(theta);
+        if value.eq(x) {
             Ok(1.0)
         } else {
             Ok(0.0)
@@ -38,9 +62,9 @@ where
 
     fn sample(
         &self,
-        _theta: &Self::Condition,
+        theta: &Self::Condition,
         _rng: &mut dyn rand::RngCore,
     ) -> Result<Self::Value, crate::DistributionError> {
-        Ok(self.value.clone())
+        Ok((self.f)(theta))
     }
 }
