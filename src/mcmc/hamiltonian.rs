2980765546cc064580c246acfc0c34aaa9beaf6d
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-12 22:16:48 +0900

    feat: move one step

--- src/mcmc/hamiltonian.rs
+++ src/mcmc/hamiltonian.rs
@@ -31,31 +31,36 @@ where
 
     pub fn sample(
         &self,
-        iter: usize,
-        initial: B,
+        x: f64,
+        max_iter: usize,
         rng: &mut dyn RngCore,
-    ) -> Result<B, DistributionError> {
-        let mut state = initial;
-        let mut count = 0;
+    ) -> Result<f64, DistributionError> {
+        let mut st = 0.0;
+        let mut ed = 1.0;
 
-        while count < iter {
-            let candidate = self.proposal.sample(&state, rng)?;
-            let r = (self.likelihood.fk(self.value, &candidate)?
-                * self.prior.fk(&candidate, &())?
-                * self.proposal.fk(&state, &candidate)?)
-                / (self.likelihood.fk(self.value, &state)?
-                    * self.prior.fk(&state, &())?
-                    * self.proposal.fk(&candidate, &state)?);
-            let r = r.min(1.0);
-            let p = rng.gen_range(0.0..=1.0);
+        let r = shrink(x)?;
+        let slice = self.likelihood.fk(&self.value, &x)? * self.prior.fk(&x, &())?
+            - 2.0 * rng.gen_range(0.0f64..1.0f64).ln();
 
-            if p < r {
-                state = candidate;
-                count += 1;
+        for _iter in 0..max_iter {
+            let rnew = rng.gen_range(st..ed);
+            let expanded = expand(rnew)?;
+
+            let newlik = self.likelihood.fk(&self.value, &expanded)?
+                * self.prior.fk(&expanded, &())?
+                - (2.0 * rnew * (1.0 - rnew));
+
+            if newlik > slice {
+                return expand(rnew);
+            } else if rnew > r {
+                ed = rnew;
+            } else if rnew < r {
+                st = rnew;
+            } else {
+                return Ok(x);
             }
         }
-
-        Ok(state)
+        Ok(x)
     }
 }
 
@@ -65,18 +70,30 @@ fn hamiltonian(p: f64, theta: f64) -> Result<f64, DistributionError> {
     Ok(lambda * theta - (alpha - 1.0) * theta.ln() + 0.5 * p.powf(2.0))
 }
 
-fn leapfrog_next_half(p: f64, theta: f64) -> Result<f64, DistributionError> {
+fn leapfrog_next_half_p(p: f64, theta: f64) -> Result<f64, DistributionError> {
     let lambda = 1.0;
     let alpha = 1.0;
-    Ok(p - 0.5 * 0.01 * (lambda - (alpha - 1.0) / theta))
+    let eps = 0.01;
+    Ok(p - 0.5 * eps * (lambda - (alpha - 1.0) / theta))
 }
 
-fn leapfrog_next(p: f64, theta: f64) -> Result<f64, DistributionError> {
-    Ok(theta + 0.01 * p)
+fn leapfrog_next_theta(p: f64, theta: f64) -> Result<f64, DistributionError> {
+    let eps = 0.01;
+    Ok(theta + eps * p)
 }
 
-// fn move_one_step(p: f64, theta: f64) -> Result<Vec<X>, DistributionError> {
-//     let L = 100.0;
-//     let p_sample = vec![];
-//     p_sample.append(1, p, theta, hamiltonian(p, theta));
-// }
+fn move_one_step(p: f64, theta: f64) -> Result<Vec<f64>, DistributionError> {
+    let L = 100;
+    let p_sample = vec![];
+    p_sample.append(&mut vec![1.0, p, theta, hamiltonian(p, theta)?]);
+    for _i in 0..L {
+        p = leapfrog_next_half_p(p, theta)?;
+        theta = leapfrog_next_theta(p, theta)?;
+        p = leapfrog_next_half_p(p, theta)?;
+        p_sample.append(&mut vec![1.0, p, theta, hamiltonian(p, theta)?])
+    }
+    Ok(p_sample)
+}
+
+// r=exp⁡(H(θ(t),p(t))−H(θ(a),p(a)))
+// これと標準正規分布を比較して受容、非受容を決定
