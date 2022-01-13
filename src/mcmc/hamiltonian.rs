2ebeac6d1276ef73a8ef0c667feb6c479e21bbf1
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-13 22:59:06 +0900

    feat: hamiltonian

--- src/mcmc/hamiltonian.rs
+++ src/mcmc/hamiltonian.rs
@@ -1,66 +1,61 @@
-use crate::{Distribution, DistributionError, RandomVariable};
+use crate::{ContinuousUniform, Distribution, DistributionError, RandomVariable};
 use rand::prelude::*;
 
 /// Sample b from posterior p(b|a) with likelihood p(a|b) and prior p(b)
-pub struct HamiltonianSampler<'a, L, P, A, B>
+pub struct HamiltonianSampler<T, D>
 where
-    L: Distribution<Value = A, Condition = B>,
-    P: Distribution<Value = B, Condition = ()>,
-    A: RandomVariable,
-    B: RandomVariable,
+    T: RandomVariable,
+    D: Distribution<Value = T, Condition = ()>,
 {
-    value: &'a A,
-    likelihood: &'a L,
-    prior: &'a P,
+    distribution: D,
 }
 
-impl<'a, L, P, A, B> HamiltonianSampler<'a, L, P, A, B>
+#[derive(thiserror::Error, Debug)]
+pub enum ImportanceSamplingError {
+    #[error("out of range")]
+    OutOfRange,
+    #[error("Unknown error")]
+    Unknown,
+}
+
+impl<T, D> HamiltonianSampler<T, D>
 where
-    L: Distribution<Value = A, Condition = B>,
-    P: Distribution<Value = B, Condition = ()>,
-    A: RandomVariable,
-    B: RandomVariable,
+    T: RandomVariable,
+    D: Distribution<Value = T, Condition = ()>,
 {
-    pub fn new(value: &'a A, likelihood: &'a L, prior: &'a P) -> Self {
-        Self {
-            value,
-            likelihood,
-            prior,
-        }
+    pub fn new(distribution: D) -> Result<Self, DistributionError> {
+        Ok(Self { distribution })
     }
 
+    // r=exp⁡(H(θ(t),p(t))−H(θ(a),p(a)))
+    // これと標準正規分布を比較して受容、非受容を決定
     pub fn sample(
         &self,
-        x: f64,
-        max_iter: usize,
+        theta: f64, //D::Condition,
         rng: &mut dyn RngCore,
-    ) -> Result<f64, DistributionError> {
-        let mut st = 0.0;
-        let mut ed = 1.0;
-
-        let r = shrink(x)?;
-        let slice = self.likelihood.fk(&self.value, &x)? * self.prior.fk(&x, &())?
-            - 2.0 * rng.gen_range(0.0f64..1.0f64).ln();
-
-        for _iter in 0..max_iter {
-            let rnew = rng.gen_range(st..ed);
-            let expanded = expand(rnew)?;
-
-            let newlik = self.likelihood.fk(&self.value, &expanded)?
-                * self.prior.fk(&expanded, &())?
-                - (2.0 * rnew * (1.0 - rnew));
-
-            if newlik > slice {
-                return expand(rnew);
-            } else if rnew > r {
-                ed = rnew;
-            } else if rnew < r {
-                st = rnew;
-            } else {
-                return Ok(x);
+    ) -> Result<Vec<f64>, DistributionError> {
+        const L: usize = 100;
+        const T: usize = 10000;
+        let mut kinetic_p = 0.0;
+        let mut kinetic_theta = theta;
+        let mut sim_result_hamiltonian = vec![];
+        let prev_hamiltonian = hamiltonian(kinetic_p, theta)?;
+        sim_result_hamiltonian.append(&mut vec![prev_hamiltonian]);
+        for _t in 0..T {
+            for _i in 0..L {
+                kinetic_p = leapfrog_next_half_p(kinetic_p, kinetic_theta)?;
+                kinetic_theta = leapfrog_next_theta(kinetic_p, kinetic_theta)?;
+                kinetic_p = leapfrog_next_half_p(kinetic_p, kinetic_theta)?;
+            }
+            let hamiltonian = hamiltonian(kinetic_p, kinetic_theta)?;
+            let r = (prev_hamiltonian - hamiltonian).exp();
+            if r > 1.0 {
+                sim_result_hamiltonian.append(&mut vec![prev_hamiltonian]);
+            } else if r > 0.0 && r > ContinuousUniform.sample(&(0.0..1.0), rng)? {
+                sim_result_hamiltonian.append(&mut vec![prev_hamiltonian]);
             }
         }
-        Ok(x)
+        Ok(sim_result_hamiltonian)
     }
 }
 
@@ -82,18 +77,28 @@ fn leapfrog_next_theta(p: f64, theta: f64) -> Result<f64, DistributionError> {
     Ok(theta + eps * p)
 }
 
-fn move_one_step(p: f64, theta: f64) -> Result<Vec<f64>, DistributionError> {
-    let L = 100;
-    let p_sample = vec![];
-    p_sample.append(&mut vec![1.0, p, theta, hamiltonian(p, theta)?]);
-    for _i in 0..L {
-        p = leapfrog_next_half_p(p, theta)?;
-        theta = leapfrog_next_theta(p, theta)?;
-        p = leapfrog_next_half_p(p, theta)?;
-        p_sample.append(&mut vec![1.0, p, theta, hamiltonian(p, theta)?])
-    }
-    Ok(p_sample)
-}
+// fn move_one_step(p: f64, theta: f64) -> Result<Vec<f64>, DistributionError> {
+//     const L: usize = 100;
+//     let mut kinetic_p = p;
+//     let mut kinetic_theta = theta;
+//     let mut p_sample_hamiltonian = vec![];
+//     p_sample_hamiltonian.append(&mut vec![hamiltonian(p, theta)?]);
+//     for _i in 0..L {
+//         kinetic_p = leapfrog_next_half_p(kinetic_p, kinetic_theta)?;
+//         kinetic_theta = leapfrog_next_theta(kinetic_p, kinetic_theta)?;
+//         kinetic_p = leapfrog_next_half_p(kinetic_p, kinetic_theta)?;
+//         p_sample_hamiltonian.append(&mut vec![hamiltonian(p, theta)?])
+//     }
+//     Ok(p_sample_hamiltonian)
+// }
 
-// r=exp⁡(H(θ(t),p(t))−H(θ(a),p(a)))
-// これと標準正規分布を比較して受容、非受容を決定
+#[cfg(test)]
+mod test {
+    use crate::{HamiltonianSampler, Normal};
+
+    #[test]
+    fn it_works() {
+        let theta = 2.5;
+        let sample = HamiltonianSampler::new(Normal).sample();
+    }
+}
