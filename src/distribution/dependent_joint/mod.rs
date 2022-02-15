580f43d8597ce33ea99271adc545193ad76cf0a9
Rubens <91782701+tibetsou@users.noreply.github.com>
2022-02-15 15:33:45 +0900

    feat: pub (crate)に修正

--- src/distribution/dependent_joint/mod.rs
+++ src/distribution/dependent_joint/mod.rs
@@ -13,8 +13,8 @@ where
     UL: Event,
     UR: Event,
 {
-    pub lhs: L,
-    pub rhs: R,
+    pub (crate) lhs: L,
+    pub (crate) rhs: R,
 }
 
 impl<L, R, T, UL, UR> DependentJoint<L, R, T, UL, UR>
