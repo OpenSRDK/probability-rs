442ee96096bece3830fdd519c020a85c3d1aeb31
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-01-31 23:05:20 +0900

    feat: ucb

--- src/nonparametric/elliptical_process/bayesian_optimization/expected_improvement.rs
+++ src/nonparametric/elliptical_process/bayesian_optimization/expected_improvement.rs
@@ -1,9 +1,16 @@
 use super::AcquisitionFunctions;
+use rand_distr::StandardNormal;
 
 pub struct ExpectedImprovement;
 
 impl AcquisitionFunctions for ExpectedImprovement {
-    fn value(&self, theta: &crate::NormalParams) -> f64 {
+    fn value(&self, theta: &crate::NormalParams, f_vec: Vec<f64>, xs: f64) -> f64 {
+        let mu = theta.mu();
+        let sigma = theta.sigma();
+        let tau = f_vec.iter().max().unwrap();
+        let f = todo!();
+        let t = (mu - tau) / sigma;
+
         todo!()
     }
 }
--- src/nonparametric/elliptical_process/bayesian_optimization/upper_confidence_bound.rs
+++ src/nonparametric/elliptical_process/bayesian_optimization/upper_confidence_bound.rs
@@ -1,9 +1,16 @@
+use num_integer::sqrt;
+
 use super::AcquisitionFunctions;
 
 pub struct UpperConfidenceBound;
 
 impl AcquisitionFunctions for UpperConfidenceBound {
-    fn value(&self, theta: &crate::NormalParams) -> f64 {
-        todo!()
+    fn value(&self, theta: &crate::NormalParams, n: usize) -> f64 {
+        let mu = theta.mu();
+        let sigma = theta.sigma();
+        let n = n as f64;
+        let k = sqrt((n.ln() / n));
+
+        mu + k * sigma
     }
 }
