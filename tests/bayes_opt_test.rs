920d226d130874e7e304ca026ba648cf991ba95f
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-02-13 23:12:53 +0900

    feat: test

--- tests/bayes_opt_test.rs
+++ tests/bayes_opt_test.rs
@@ -7,12 +7,9 @@ extern crate opensrdk_probability;
 use crate::opensrdk_probability::*;
 use cmaes::{fmax, DVector};
 pub use expected_improvement::*;
-use opensrdk_kernel_method::*;
 use opensrdk_kernel_method::{Periodic, PositiveDefiniteKernel, RBF};
 use opensrdk_probability::nonparametric::*;
-use rand::prelude::*;
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use rand_distr::StandardNormal;
 
 struct Data {
     x_history: Vec<Vec<f64>>,
@@ -55,7 +52,7 @@ fn test_main() {
     }
 
     loop {
-        let xs = vec![maximize_ucb(&data, n)];
+        let xs = maximize_ucb(&data, n);
         // let xs = maximize_ei(&data);
 
         sampling(&mut data, xs);
@@ -65,7 +62,6 @@ fn test_main() {
 }
 
 fn objective(_x: &Vec<f64>) -> f64 {
-    // x + x.powf(2.0)
     todo!()
 }
 
@@ -85,53 +81,56 @@ fn gp_regression(x: Vec<Vec<f64>>, y: &Vec<f64>, xs: &Vec<f64>) -> NormalParams 
     let mu = params_y.gp_predict(xs).unwrap().mu();
     let sigma = params_y.gp_predict(xs).unwrap().sigma();
 
-    NormalParams { mu, sigma }
+    NormalParams::new(mu, sigma).unwrap()
 }
 
-fn calc_ucb(data: &Data, n: usize, xs: &Vec<f64>) -> f64 {
-    let theta: NormalParams = gp_regression(data.x_history, &data.y_history, &xs);
-    let ucb = UpperConfidenceBound { trial: n };
-    ucb.value(&theta)
-}
-
-fn calc_ei(data: &Data, n: usize, xs: &Vec<f64>) -> f64 {
-    let theta: NormalParams = gp_regression(&data.x_history, &data.y_history, &xs);
-    let ei = ExpectedImprovement {
-        f_vec: data.y_history,
-    };
-    ei.value(&theta)
-}
-
-fn maximize_ucb(data: &Data, n: usize) -> f64 {
+fn maximize_ucb(data: &Data, n: usize) -> Vec<f64> {
     let func_to_maximize = |xs: Vec<f64>| {
-        let theta: NormalParams = gp_regression(&data.x_history, &data.y_history, &xs);
+        let theta: NormalParams = gp_regression(data.x_history.clone(), &data.y_history, &xs);
         let ucb = UpperConfidenceBound { trial: n };
         ucb.value(&theta)
     };
+    let mut mean_x: Vec<f64> = vec![];
+
+    for k in 1..data.y_history.len() {
+        let xk = data.x_history.iter().map(|x| x[k]).collect::<Vec<f64>>();
+        let sum_xk: f64 = xk.iter().sum();
+        let mean_xk = sum_xk / xk.len() as f64;
+        mean_x.push(mean_xk);
+    }
 
     let solution = fmax(
-        |x: &DVector<f64>| func_to_maximize(x),
-        data.x_history.to_vec(),
+        |x: &DVector<f64>| func_to_maximize(x.iter().cloned().collect::<Vec<_>>()),
+        mean_x,
         0.01,
     );
-    let xs = solution.point[0];
+    let xs = solution.point.iter().cloned().collect::<Vec<_>>();
     xs
 }
 
-fn maximize_ei(data: &Data, n: usize) -> f64 {
+fn maximize_ei(data: &Data, n: usize) -> Vec<f64> {
     let func_to_maximize = |xs: Vec<f64>| {
-        let theta: NormalParams = gp_regression(&data.x_history, &data.y_history, &xs);
+        let theta: NormalParams = gp_regression(data.x_history.clone(), &data.y_history, &xs);
         let ei = ExpectedImprovement {
-            f_vec: data.y_history,
+            f_vec: data.y_history.clone(),
         };
         ei.value(&theta)
     };
 
+    let mut mean_x: Vec<f64> = vec![];
+
+    for k in 1..data.y_history.len() {
+        let xk = data.x_history.iter().map(|x| x[k]).collect::<Vec<f64>>();
+        let sum_xk: f64 = xk.iter().sum();
+        let mean_xk = sum_xk / xk.len() as f64;
+        mean_x.push(mean_xk);
+    }
+
     let solution = fmax(
-        |x: &DVector<f64>| func_to_maximize(x),
-        data.x_history.to_vec(),
+        |x: &DVector<f64>| func_to_maximize(x.iter().cloned().collect::<Vec<_>>()),
+        mean_x,
         0.01,
     );
-    let xs = solution.point[0];
+    let xs = solution.point.iter().cloned().collect::<Vec<_>>();
     xs
 }
