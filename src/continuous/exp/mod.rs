0756bc70d57e49d943fc8b92fcb35ae34c3f2ccd
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-04-25 22:40:36 +0900

    feat: exponential distribution

--- src/continuous/exp/mod.rs
+++ src/continuous/exp/mod.rs
@@ -1,79 +1,74 @@
-// use crate::{DependentJoint, Distribution, IndependentJoint, RandomVariable};
-// use crate::{DistributionError, SamplableDistribution};
-// use rand::prelude::*;
-// use rand_distr::Exp as RandExp;
-// use std::{ops::BitAnd, ops::Mul};
+use crate::{ContinuousDistribution, JointDistribution};
+use crate::{DistributionError, EllipticalParams};
+use opensrdk_kernel_method::PositiveDefiniteKernel;
+use opensrdk_linear_algebra::{DiagonalMatrix, Matrix, SymmetricPackedMatrix, Vector};
+use opensrdk_symbolic_computation::{Expression, Size};
+use rand::prelude::*;
+use rand_distr::StandardNormal;
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+use std::ops::BitAnd;
+use std::{collections::HashSet, f64::consts::PI, ops::Mul};
 
-// pub mod params;
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ExponentialDistribution {
+    x: Expression,
+    lambda: Expression,
+}
 
-// pub use params::*;
+impl ExponentialDistribution {
+    pub fn new(x: Expression, lambda: Expression, d: usize) -> ExponentialDistribution {
+        if x.mathematical_sizes() != vec![Size::Many, Size::One] && x.mathematical_sizes() != vec![]
+        {
+            panic!("x must be a scalar or a 2 rank vector");
+        }
+        ExponentialDistribution { x, lambda }
+    }
+}
 
-// /// Exponential distribution
-// #[derive(Clone, Debug)]
-// pub struct Exp;
+impl<Rhs> Mul<Rhs> for ExponentialDistribution
+where
+    Rhs: ContinuousDistribution,
+{
+    type Output = JointDistribution<Self, Rhs>;
 
-// #[derive(thiserror::Error, Debug)]
-// pub enum ExpError {
-//     #[error("Lambda must be positive")]
-//     LambdaMustBePositive,
-// }
+    fn mul(self, rhs: Rhs) -> Self::Output {
+        JointDistribution::new(self, rhs)
+    }
+}
 
-// impl Distribution for Exp {
-//     type Value = f64;
-//     type Condition = ExpParams;
+impl ContinuousDistribution for ExponentialDistribution {
+    fn value_ids(&self) -> HashSet<&str> {
+        self.x.variable_ids()
+    }
 
-//     fn p_kernel(&self, x: &Self::Value, theta: &Self::Condition) -> Result<f64, DistributionError> {
-//         let lambda = theta.lambda();
+    fn conditions(&self) -> Vec<&Expression> {
+        vec![&self.lambda]
+    }
 
-//         Ok(lambda * (-lambda * x).exp())
-//     }
-// }
+    fn pdf(&self) -> Expression {
+        let x = self.x.clone();
+        if x < 0.0 {
+            return 0.0.into();
+        }
+        let lambda = self.lambda.clone();
+        let pdf_expression = lambda * (-lambda * x).exp();
 
-// impl<Rhs, TRhs> Mul<Rhs> for Exp
-// where
-//     Rhs: Distribution<Value = TRhs, Condition = ExpParams>,
-//     TRhs: RandomVariable,
-// {
-//     type Output = IndependentJoint<Self, Rhs, f64, TRhs, ExpParams>;
+        pdf_expression
+    }
 
-//     fn mul(self, rhs: Rhs) -> Self::Output {
-//         IndependentJoint::new(self, rhs)
-//     }
-// }
+    fn condition_ids(&self) -> HashSet<&str> {
+        self.conditions()
+            .iter()
+            .map(|v| v.variable_ids())
+            .flatten()
+            .collect::<HashSet<_>>()
+            .difference(&self.value_ids())
+            .cloned()
+            .collect()
+    }
 
-// impl<Rhs, URhs> BitAnd<Rhs> for Exp
-// where
-//     Rhs: Distribution<Value = ExpParams, Condition = URhs>,
-//     URhs: RandomVariable,
-// {
-//     type Output = DependentJoint<Self, Rhs, f64, ExpParams, URhs>;
-
-//     fn bitand(self, rhs: Rhs) -> Self::Output {
-//         DependentJoint::new(self, rhs)
-//     }
-// }
-
-// impl SamplableDistribution for Exp {
-//     fn sample(
-//         &self,
-//         theta: &Self::Condition,
-//         rng: &mut dyn RngCore,
-//     ) -> Result<Self::Value, DistributionError> {
-//         let lambda = theta.lambda();
-
-//         let exp = match RandExp::new(lambda) {
-//             Ok(v) => Ok(v),
-//             Err(e) => Err(DistributionError::Others(e.into())),
-//         }?;
-
-//         Ok(rng.sample(exp))
-//     }
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         assert_eq!(2 + 2, 4);
-//     }
-// }
+    fn ln_pdf(&self) -> Expression {
+        self.pdf().ln()
+    }
+}
