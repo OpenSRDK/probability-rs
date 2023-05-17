d90c485fa48183cbe50f6318c3f041564840b2fc
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-05-17 22:09:12 +0900

    feat: comment out

--- src/continuous/gamma/params.rs
+++ src/continuous/gamma/params.rs
@@ -1,48 +1,48 @@
-use crate::{DistributionError, GammaError, RandomVariable};
+// use crate::{DistributionError, GammaError, RandomVariable};
 
-#[derive(Clone, Debug, PartialEq)]
-pub struct GammaParams {
-    shape: f64,
-    scale: f64,
-}
+// #[derive(Clone, Debug, PartialEq)]
+// pub struct GammaParams {
+//     shape: f64,
+//     scale: f64,
+// }
 
-impl GammaParams {
-    pub fn new(shape: f64, scale: f64) -> Result<Self, DistributionError> {
-        if shape <= 0.0 {
-            return Err(DistributionError::InvalidParameters(
-                GammaError::ShapeMustBePositive.into(),
-            ));
-        }
-        if scale <= 0.0 {
-            return Err(DistributionError::InvalidParameters(
-                GammaError::ScaleMustBePositive.into(),
-            ));
-        }
+// impl GammaParams {
+//     pub fn new(shape: f64, scale: f64) -> Result<Self, DistributionError> {
+//         if shape <= 0.0 {
+//             return Err(DistributionError::InvalidParameters(
+//                 GammaError::ShapeMustBePositive.into(),
+//             ));
+//         }
+//         if scale <= 0.0 {
+//             return Err(DistributionError::InvalidParameters(
+//                 GammaError::ScaleMustBePositive.into(),
+//             ));
+//         }
 
-        Ok(Self { shape, scale })
-    }
+//         Ok(Self { shape, scale })
+//     }
 
-    pub fn shape(&self) -> f64 {
-        self.shape
-    }
+//     pub fn shape(&self) -> f64 {
+//         self.shape
+//     }
 
-    pub fn scale(&self) -> f64 {
-        self.scale
-    }
-}
+//     pub fn scale(&self) -> f64 {
+//         self.scale
+//     }
+// }
 
-impl RandomVariable for GammaParams {
-    type RestoreInfo = ();
+// impl RandomVariable for GammaParams {
+//     type RestoreInfo = ();
 
-    fn transform_vec(&self) -> (Vec<f64>, Self::RestoreInfo) {
-        (vec![self.shape, self.scale], ())
-    }
+//     fn transform_vec(&self) -> (Vec<f64>, Self::RestoreInfo) {
+//         (vec![self.shape, self.scale], ())
+//     }
 
-    fn len(&self) -> usize {
-        2usize
-    }
+//     fn len(&self) -> usize {
+//         2usize
+//     }
 
-    fn restore(v: &[f64], _: &Self::RestoreInfo) -> Result<Self, DistributionError> {
-        Self::new(v[0], v[1])
-    }
-}
+//     fn restore(v: &[f64], _: &Self::RestoreInfo) -> Result<Self, DistributionError> {
+//         Self::new(v[0], v[1])
+//     }
+// }
