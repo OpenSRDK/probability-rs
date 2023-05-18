f5087349370961aad23754fa6dbe799a66410fcd
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2023-05-18 23:34:04 +0900

    feat: exponential distribution

--- src/discrete/poisson/mod.rs
+++ src/discrete/poisson/mod.rs
@@ -1,3 +1,69 @@
+use crate::{DiscreteDistribution, JointDistribution};
+use opensrdk_symbolic_computation::{Expression, Size};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashSet, ops::Mul};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Poisson {
+    x: Expression,
+    lambda: Expression,
+    k: Expression,
+}
+
+impl Poisson {
+    pub fn new(x: Expression, lambda: Expression, k: Expression) -> Poisson {
+        if k.mathematical_sizes() != vec![Size::Many, Size::One] && k.mathematical_sizes() != vec![]
+        {
+            panic!("k must be a scalar or a 2 rank vector");
+        }
+        Poisson { x, lambda, k }
+    }
+}
+
+// impl<Rhs> Mul<Rhs> for Geometric
+// where
+//     Rhs: DiscreteDistribution,
+// {
+//     type Output = JointDistribution<Self, Rhs>;
+
+//     fn mul(self, rhs: Rhs) -> Self::Output {
+//         JointDistribution::new(self, rhs)
+//     }
+// }
+
+impl DiscreteDistribution for Poisson {
+    fn value_ids(&self) -> HashSet<&str> {
+        self.k.variable_ids()
+    }
+
+    fn conditions(&self) -> Vec<&Expression> {
+        vec![&self.p]
+    }
+
+    fn pmf(&self) -> Expression {
+        let k = self.k.clone();
+        let p = self.p.clone();
+        let pf_expression = p.pow(k) * (1.0 - p).pow(1.0 - k);
+
+        pf_expression
+    }
+
+    fn condition_ids(&self) -> HashSet<&str> {
+        self.conditions()
+            .iter()
+            .map(|v| v.variable_ids())
+            .flatten()
+            .collect::<HashSet<_>>()
+            .difference(&self.value_ids())
+            .cloned()
+            .collect()
+    }
+
+    fn ln_pmf(&self) -> Expression {
+        self.pmf().ln()
+    }
+}
+
 // pub mod params;
 
 // pub use params::*;
